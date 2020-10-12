b3a8fed5ea06a4aa35c4b4fae6309d0013e86f56
Macjus-ML <abolshakovy@gmail.com>
2020-10-12 23:41:12 +0300

    All fixed after review1 for loops_floyd

--- final_task_loops_floyd_contest.py
+++ final_task_loops_floyd_contest.py
@@ -1,14 +1,4 @@
-# Номер успешной посылки 36138548 - 12 окт 2020, 23:15:24
-
-class Node:
-
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-    def __repr__(self):
-        return str(self.value)
-
+# Номер успешной отправки 36139292 - 12 окт 2020, 23:37:58
 
 def hasCycle(head):
     """
