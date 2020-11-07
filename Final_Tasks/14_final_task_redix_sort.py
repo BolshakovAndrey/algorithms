8ed64e5c744ef5f8fcacc2cb60ec5bf6f402a88e
Macjus-ML <abolshakovy@gmail.com>
2020-11-07 16:13:01 +0300

    Tuned after review1

--- Final_Tasks/14_final_task_redix_sort.py
+++ Final_Tasks/14_final_task_redix_sort.py
@@ -1,27 +1,29 @@
 # Номер  успешной отправки - 39300502 - 4 ноя 2020, 22:25:22
-n = int(input())
-arr = [p for p in input().split(' ')]
 
-# max_len — number of digits in the longest key;
-max_len = len(max(arr, key=len))
+def solution(length, data):
+    for exponent in range(length):
+        digits = [[] for i in range(10)]  # create buckets for each digit
+        index = -(exponent + 1)  # get the string index for the number
 
-# Use an iterative approach based on LSD - it's faster
+        for number in data:  # placing numbers into a bucket based on LSD
+            string_number = str(number)
+            try:
+                digit = int(string_number[index])  # getting digit at current place
+            except IndexError:
+                digit = 0  # if number doesn't have this digit it is 0
+            digits[digit].append(number)
 
-sort_arr = arr[:]
-for exponent in range(max_len):
-    digits = [[] for i in range(10)]  # create buckets for each digit
-    index = -(exponent + 1)  # get the string index for the number
+        data = []
+        for numeral in digits:
+            data.extend(numeral)
+    return data
 
-    for number in sort_arr:  # placing numbers into a bucket based on LSD
-        string_number = str(number)
-        try:
-            digit = int(string_number[index])  # getting digit at current place
-        except IndexError:
-            digit = 0  # if number doesn't have this digit it is 0
-        digits[digit].append(number)
 
-    sort_arr = []
-    for numeral in digits:
-        sort_arr.extend(numeral)
-
-print(*sort_arr)
+if __name__ == '__main__':
+    n = int(input())
+    arr = [p for p in input().split(' ')]
+    # max_len — number of digits in the longest key;
+    max_len = len(max(arr, key=len))
+    sort_arr = arr[:]
+    answer = solution(max_len, sort_arr)
+    print(*answer)
